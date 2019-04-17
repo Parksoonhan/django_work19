6a2953868cba5cbd3d86231e5cc14c7c45d883c7
Parksoonhan <qkrtnsgks38@gmail.com>
2019-04-18 00:45:26 +0900

    My Django App_2

--- prj/settings.py
+++ prj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l$#a4a%)pe=!e=dncp2gax%**wv3!fz&(t*hpvg-me=l^yk4i1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['Parksoonhan.pythonanywhere.com']
 
